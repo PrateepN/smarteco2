b400aeadb4fbc9924c22f07987adace106a064c5
PrateepN <31596845+PrateepN@users.noreply.github.com>
2017-09-05 16:55:44 +0530

    Add files via upload

--- demo.py
+++ demo.py
@@ -2,26 +2,18 @@ import logging
 
 from flask import Flask
 from flask_ask import Ask, statement, question
-from flask import json
+from flask import jsonify
 
 app = Flask(__name__)
 @app.route('/webhook2', methods=['POST'])
 ask = Ask(app, "/")
 @ask.launch
 def launch():
-    return statement("Welcome to the requests demo")
+    return statement(jsonify({'speech':"Welcome to the requests demo"}))
 
 speech="We are working on that"
 	
-@ask.intent("Premium_amount")
-def hello():
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-		speech: speech,
-        mimetype='application/json'
-    )
-    return response
+
 
 	
 if __name__ == '__main__':
