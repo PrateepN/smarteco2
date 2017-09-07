200e7c1b1b6b5440762098c9239b97c9a28259ba
PrateepN <31596845+PrateepN@users.noreply.github.com>
2017-09-07 10:24:42 +0530

    Add files via upload

--- demo.py
+++ demo.py
@@ -12,18 +12,15 @@ from flask_ask import Ask, statement, question
 
 app = Flask(__name__)
 @app.route('/webhook2', methods=['POST'])
-ask = Ask(app, "/")
-@ask.launch
-def launch():
-    return statement(jsonify({'speech':"Welcome to the requests demo"}))
 
-speech="We are working on that"
-	
-
+def login():
+		if request.method == 'POST':
+        test = request.data
+        return test
 
 	
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8000))
 
     print("Starting app on port %d" % port)
 
